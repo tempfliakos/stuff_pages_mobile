4096e59b806828fd41c555cbecd597f93ee2d5ba
Tempfli Ákos <akos.tempfli@telcotrend.hu>
2022-05-16 13:56:52 +0200

    eles

--- lib/request/http.dart
+++ lib/request/http.dart
@@ -4,8 +4,8 @@ import 'package:http/http.dart' as http;
 
 import '../global.dart';
 
-const URL = 'http://localhost:3001/';
-//const URL = 'https://stuff-pages-server.herokuapp.com/';
+//const URL = 'http://localhost:3001/';
+const URL = 'https://stuff-pages-server.herokuapp.com/';
 
 class Api {
   static Future get(String endpoint) {
